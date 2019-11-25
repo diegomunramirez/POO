7364ffb320a61dfd25210ff53b36a67538c5ddbb
diegomunramirez <57073455+diegomunramirez@users.noreply.github.com>
2019-11-24 20:22:39 -0600

    subir clases e imagenes

--- Ajedrez/src/ajedrez/Rey.java
+++ Ajedrez/src/ajedrez/Rey.java
@@ -4,17 +4,17 @@ import javax.swing.ImageIcon;
 import java.awt.Image;
 
 public class Rey implements Pieza {
-    public static String directorioDeImagen = "src/ajedrez/rey_imagen.png";
+    public static String directorioDeImagen = "src/ajedrez/rey_imagen.png"; //se pone la direccion de la imagen 
 
     @Override
-    public boolean validaMovimiento(PosicionTablero origen, PosicionTablero destino) {
+    public boolean validaMovimiento(PosicionTablero origen, PosicionTablero destino, Pieza[][] tablero) {
         return Math.abs(origen.getColumna() - destino.getColumna()) <= 1 &&
-               Math.abs(origen.getFila() - destino.getFila()) <= 1;
+               Math.abs(origen.getFila() - destino.getFila()) <= 1; //valida las reglas del juego del rey, dice si se puede mover
     }
 
     @Override
-    public ImageIcon obtenerImagen(int ancho, int alto) {
-        //ajustar la imagen a las dimensiones del boton
+    public ImageIcon obtenerImagen(int ancho, int alto) { //le da las dimensiones a la imagen (la ajusta a los label)
+        //ajustar la imagen a las dimensiones del label
         return new ImageIcon(new ImageIcon(directorioDeImagen).getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH));
     }
 }
